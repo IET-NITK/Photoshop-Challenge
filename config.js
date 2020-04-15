6f4e6a246b1419689a4ea480890f93c732bed5c2
26prajval98 <26prajval98@gmail.com>
2020-04-15 15:14:56 +0530

    added config and made repo private

---
+++ config.js
@@ -0,0 +1,21 @@
+var db = {
+    username : "iet",
+    password : "ietnitk2018",
+    uri :   "mongodb://iet:ietnitk2018@ds119445.mlab.com:19445/photoshop"
+}
+
+var email = {
+    username : "dev.ietnitk@gmail.com",
+    password : "ietnitk2018"
+}
+
+var logs = {
+    dbsuccess : "SUCCESSFULLY CONNECTED TO DATABASE",
+    dberr : "ERROR IN CONNECTING TO DATABASE",
+}
+
+module.exports = {
+    db,
+    email,
+    logs
+}
